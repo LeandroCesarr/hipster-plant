0ea48a7c76db12232b57a1b9e37ec8142228cb26
Leandro C. Silva <leandrocesar49@hotmail.com>
2023-07-22 11:35:28 -0300

    feat: adds main file

---
+++ main/main.ino
@@ -0,0 +1,19 @@
+/*
+  Project: Hipster Plant
+  Description: This code contains main functions for a smart plant project with sensors and actuators.
+
+  Autor: Leandro C. Silva
+  Data: 22/07/2023
+*/
+
+/*
+  Entry point
+*/
+void setup() {
+}
+
+/*
+  Loop
+*/
+void loop() {
+}
