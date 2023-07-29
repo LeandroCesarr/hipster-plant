37cf1a44257514df56ae3292edd6ec8d7f3feefe
Leandro C. Silva <leandrocesar49@hotmail.com>
2023-07-29 19:11:43 -0300

    feat: Adds blynk setup
    adds IoT Blynk platform setup

--- main/main.ino
+++ main/main.ino
@@ -6,19 +6,38 @@
   Data: 22/07/2023
 */
 
-/* Libraries includes */
-/*                    */
-
 #include "main_env.h"
+
+/* Constants */
+#define BLYNK_PRINT Serial
+#define ESP8266_BAUD 9600
+
+/* Libraries includes */
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial EspSerial(2, 3); // TX, RX
+ESP8266 ESP8266_MODULE(&EspSerial);
 
 /*
   Entry point
 */
 void setup() {
+  // Debug console
+  Serial.begin(ESP8266_BAUD);
+  setup_blynk();
 }
 
 /*
   Loop
 */
 void loop() {
+  Blynk.run();
+}
+
+void setup_blynk() {
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ESP8266_MODULE, WIFI_SSID, WIFI_PASS);
 }
