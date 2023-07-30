7f7e02c860be755e1062d6d94cb6f0215d055a12
Leandro C. Silva <leandrocesar49@hotmail.com>
2023-07-29 23:21:36 -0300

    feat: adds dht sensor
    adds dht sensor, sending temperature data to cloud

--- main/main.ino
+++ main/main.ino
@@ -14,15 +14,18 @@
 #define SEND_DATA_INTERVAL_IN_SECONDS 2
 
 #define MOISTURE_PORT A0
+#define DHT_PORT 7
 
 /* Libraries includes */
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 #include <SoftwareSerial.h>
+#include <DHT11.h>
 
 BlynkTimer timer;
 SoftwareSerial EspSerial(2, 3); // TX, RX
 ESP8266 ESP8266_MODULE(&EspSerial);
+DHT11 dht(DHT_PORT);
 
 /*
   Entry point
@@ -44,6 +47,7 @@ void loop() {
 
 void setup_ports() {
   pinMode(MOISTURE_PORT, INPUT);
+  pinMode(DHT_PORT, INPUT);
 }
 
 void setup_blynk() {
@@ -56,14 +60,21 @@ void setup_blynk() {
 
 void send_data_to_blynk() {
   int current_moisture = get_moisture_percentage();
+  int current_temperature = get_temperature();
 
   Blynk.virtualWrite(V0, current_moisture);
+  Blynk.virtualWrite(V1, current_temperature);
   Serial.println("Current moisture: " + String(current_moisture) + "%");
+  Serial.println("Current temperature: " + String(current_temperature) + " C");
   Serial.println();
 }
 
 int get_moisture_percentage() {
   return parse_analog_read_to_percentage(analogRead(MOISTURE_PORT));
+}
+
+int get_temperature() {
+  return dht.readTemperature();
 }
 
 int parse_analog_read_to_percentage(int value) {
