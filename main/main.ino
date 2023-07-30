6b2d4773667ecee63908b914627afa616ba49b96
Leandro C. Silva <leandrocesar49@hotmail.com>
2023-07-29 23:18:52 -0300

    refactor: adds blynk timer
    adds method to send data to cloud

--- main/main.ino
+++ main/main.ino
@@ -11,12 +11,14 @@
 /* Constants */
 #define BLYNK_PRINT Serial
 #define ESP8266_BAUD 9600
+#define SEND_DATA_INTERVAL_IN_SECONDS 2
 
 /* Libraries includes */
 #include <ESP8266_Lib.h>
 #include <BlynkSimpleShieldEsp8266.h>
 #include <SoftwareSerial.h>
 
+BlynkTimer timer;
 SoftwareSerial EspSerial(2, 3); // TX, RX
 ESP8266 ESP8266_MODULE(&EspSerial);
 
@@ -34,10 +36,15 @@ void setup() {
 */
 void loop() {
   Blynk.run();
+  timer.run();
 }
 
 void setup_blynk() {
   EspSerial.begin(ESP8266_BAUD);
   delay(10);
   Blynk.begin(BLYNK_AUTH_TOKEN, ESP8266_MODULE, WIFI_SSID, WIFI_PASS);
+  delay(10);
+  timer.setInterval(1000L * SEND_DATA_INTERVAL_IN_SECONDS, send_data_to_blynk);
 }
+
+void send_data_to_blynk() {}
